e9dcd0e99fb8f77bca4e2ec087b7a730c39f2695
pciruelos <pciruelos@yahoo.com>
2024-04-10 12:48:13 +0200

    small practice with jest and supertest

--- test/index.test.js
+++ test/index.test.js
@@ -5,5 +5,50 @@ describe('GET /task', () => {
     test('should respond with 200 status code', async() => {
         const response = await request(app).get('/task').send()
         expect(response.statusCode).toBe(200)
+    });
+
+    test('should respond with an array', async() => {
+        const response = await request(app).get('/task').send()
+        expect(response.body).toBeInstanceOf(Array)
     })
-})
+});
+
+describe('POST /tasks', () => {
+    describe('given a title and description',  () => {
+
+        const newTask = {
+            title: "test task",
+            description: "test description"
+        }
+
+        test('should respond with 200 status code', async() => {
+            const response = await request(app).post('/tasks').send(newTask)
+            expect(response.statusCode).toBe(200)
+        });
+    
+        test('should be an object', async() => {
+            const response = await request(app).post('/tasks').send(newTask)
+            expect(response.headers['content-type']).toEqual(
+                expect.stringContaining("json"))
+        })
+    
+        test('id must exist', async() => {
+            const response = await request(app).post('/tasks').send(newTask);
+            expect(response.body.id).toBeDefined();
+        })
+    })
+
+    describe('when title is missing or description is missing', () => {
+        test('should respond with a 400 status code', async() => {
+            const fields = [
+                {},
+                {title: 'test'},
+                {description: 'descp'},
+            ]
+            for (const body of fields) {
+                const response = await request(app).post('/tasks').send(body)
+                expect(response.statusCode).toBe(400)
+            }
+        })
+    })
+});
